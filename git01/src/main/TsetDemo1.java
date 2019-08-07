ef0249208f42dbef8b3cbcff1fcc501bb7d4595e
dumingliang <2500427706@qq.com>
2019-08-07 17:10:55 +0800

    Add files via upload

---
+++ git01/src/main/TsetDemo1.java
@@ -0,0 +1,16 @@
+package main;
+
+import static java.lang.System.out;
+
+/**
+ * Created by IntelliJ IDEA
+ *
+ * @auther XXX
+ * @date 2019/8/7
+ * @time 17:00
+ */
+public class TsetDemo1 {
+    public static void main(String[] args) {
+        out.println("hello git");
+    }
+}
---
+++ git01/src/main/servlet/MyServlet.java
@@ -0,0 +1,11 @@
+package main.servlet;
+
+/**
+ * Created by IntelliJ IDEA
+ *
+ * @auther XXX
+ * @date 2019/8/7
+ * @time 17:09
+ */
+public class MyServlet {
+}
